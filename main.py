54a837d4d047b0dfb8e094fbda4cacaa6c96f9d9
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-10-25 18:16:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+print("Your band name is " + city_name + " "+ pet_name)
+
